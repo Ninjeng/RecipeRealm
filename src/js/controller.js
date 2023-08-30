0ba0f6941d05d3df0bc6cae22045750304d768da
Ninjeng lama <49013629+Ninjeng@users.noreply.github.com>
2023-08-30 13:25:36 +1000

    Search engine added backend

---
+++ src/js/controller.js
@@ -0,0 +1,56 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+
+const recipeContainer = document.querySelector('.recipe');
+
+const constrolRecipes = async function(){
+  try{
+
+    const id = window.location.hash.slice(1);
+    if (!id) return; 
+    recipeView.renderSpinner();
+    await model.loadRecipe(id);
+  
+    recipeView.render(model.state.recipe);
+    
+    
+  }catch(err)
+  {
+    console.log(err);
+    recipeView.renderError()
+
+  }
+};
+
+const controlSearchResults = async function(){
+
+  try{
+    resultsView.renderSpinner();
+    const query = searchView.getQuery();
+    if(!query) return;
+
+    await model.loadSearchResults(query)
+    console.log(model.state.search.results)
+    resultsView.render(model.state.search.results);
+  }catch(err){
+    console.log(err)
+  }
+}
+
+
+
+const init = function ()
+{
+  recipeView.addHandlerRender(constrolRecipes);
+  searchView.addHandlerSearch(controlSearchResults)
+}
+init()
+
+
